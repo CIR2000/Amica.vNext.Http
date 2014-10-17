34110af586571362bfad5faa4059616c7ce3ddea
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-10-17 17:45:04 +0200

    Console Application (Mono)

---
+++ Playground/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Amica.vNext.Http;
+
+namespace Playground
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var rc = new RestClient ();
+			Console.WriteLine (rc.ToString());
+		}
+	}
+}
