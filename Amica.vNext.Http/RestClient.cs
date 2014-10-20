9af61e2edfac574c8607c7511e560fb239640c92
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-10-20 18:47:11 +0200

    GetAsync prototype

--- Amica.vNext.Http/RestClient.cs
+++ Amica.vNext.Http/RestClient.cs
@@ -1,8 +1,48 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Amica.vNext.Http
 {
     public class RestClient
     {
+		public RestClient(Uri baseAddress)
+		{
+			BaseAddress = baseAddress;
+		}
+		public RestClient (string baseAddress) : this (new Uri (baseAddress)) { }
+
+		public async Task<T> GetAsync<T>(string resourceName, string documentId) {
+			using (var client = new HttpClient ()) {
+				client.BaseAddress = BaseAddress;
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+				HttpResponseMessage response = await client.GetAsync(string.Format("{0}/{1}", resourceName, documentId));
+				if (response.IsSuccessStatusCode)
+				{
+					var json = await response.Content.ReadAsStringAsync ();
+					T obj = JsonConvert.DeserializeObject<T>(json);
+					return obj;
+				}
+			}
+			return default(T);
+		}
+
+		public async Task<T> GetAsync<T>() {
+			return await GetAsync<T> (ResourceName, DocumentId);
+		}
+
+		public async Task<T> GetAsync<T>(string documentId) {
+			return await GetAsync<T> (ResourceName, documentId);
+		}
+
+		public Uri BaseAddress { get; set; }
+		public string ResourceName { get; set; }
+		public string DocumentId { get; set; }
     }
+
+
 }
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Amica.vNext.Http;
 
 namespace Playground
 {
+	class Person {
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+	}
+
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			var rc = new RestClient ();
-			Console.WriteLine (rc.ToString());
-		    Console.ReadKey();
+			RunAsync ().Wait ();
 		}
+        static async Task RunAsync()
+        {
+			var rc = new RestClient ("http://eve-demo.herokuapp.com/");
+
+			Person person;
+
+//			person = await rc.GetAsync<Person> ("people", "54450894d71ddf000237ae8d");
+//			Console.WriteLine (person.LastName);
+//			Console.WriteLine (person.FirstName);
+
+			rc.ResourceName = "people";
+			person = await rc.GetAsync<Person> ("54450894d71ddf000237ae8c");
+			Console.WriteLine (person.LastName);
+			Console.WriteLine (person.FirstName);
+
+//			rc.DocumentId = "54450894d71ddf000237ae8e";
+//			person = await rc.GetAsync<Person> ();
+//			Console.WriteLine (person.LastName);
+//			Console.WriteLine (person.FirstName);
+
+
+	  }
 	}
 }
